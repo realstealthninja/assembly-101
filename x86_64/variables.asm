feb468b99131b3aa64108e22db62c1572851d5c9
realstealthninja <realstealthninja@gmail.com>
2024-01-02 11:06:44 +0530

    feat: add addition of quad words

--- x86_64/variables.asm
+++ x86_64/variables.asm
@@ -2,14 +2,15 @@
 ;
 ;  consider a register rax
 ;     this entire thing is rax
-;  <-------------------------------->
+;  <--------------rax--------------->
 ;                   <-----eax-------> 
 ;                              <-ax-> 
 ;                              <----> ax is divided into ah and al respectively 
-;  [00000000000000000000000000000000]
+;  [00000000000000000000000000000000] this is in hex containing 64 bytes
 ;
+;  remember one hex digit is 2 bytes
 ;
-; data section contains initialized variables and constants
+;  data section contains initialized variables and constants
 
 section .data
 
@@ -18,8 +19,8 @@ section .data
 EXIT_SUCCESS equ 0 ; here equ says that exit success is a constant of value 0
 EXIT_TO_SYS equ 60 ; the code to terminate the program
 ;---------- general ---------------;
-
 PI equ 3.14
+;----------------------------------;
 
 ; Following snake_case instead of hUngarianCase defined in the document
 
@@ -38,7 +39,11 @@ result_word  dw   0
 ;                  \/---- datatype of double word [ 4 bytes ]
 e                  dd  40000
 f                  dd  10000
-result_double_word dd 0
+result_double_word dd  0
+
+g                  dq  400000
+h                  dq  100000 ; 
+result_quad_word   dq  0      ; 0x00
 
 ; section which defines the code
 section .text
@@ -65,8 +70,14 @@ _start:
     add eax, dword [f]
     mov dword [result_double_word], eax
 
+    ; perform addition on quad words
+
+    mov rax, qword [g]
+    add rax, qword [h]
+    mov qword [result_quad_word], rax
+
 
 last:
-  mov rax, EXIT_TO_SYS
-  mov rdi, EXIT_SUCCESS
-  syscall
+  mov rax, EXIT_TO_SYS  ; move call code for exit to rax
+  mov rdi, EXIT_SUCCESS ; move exit status to rdi
+  syscall               ; tell kernel to call the function
